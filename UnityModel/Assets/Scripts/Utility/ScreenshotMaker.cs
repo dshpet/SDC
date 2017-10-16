aa9991a8231435a2772cb9d86d30552e3c23b7e5
dshpet <dshpet@gmail.com>
2017-10-17 01:25:28 +0300

    add unity standard assets to project. Add car to main scene. Make screenshots on F1 press

---
+++ UnityModel/Assets/Scripts/Utility/ScreenshotMaker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ScreenshotMaker : MonoBehaviour
+{
+    //
+    // Config
+    //
+
+    public string ScreenshotsFolder = "Screenshots";
+    public int    Scale             = 1;
+
+    //
+    // Members
+    //
+
+    private int m_ScreenshotsTaken = 0;
+
+    //
+    // Functions
+    //
+
+	void Update()
+    {
+		if (Input.GetKeyUp(KeyCode.F1))
+        {
+            string ScreenshotsPath = Application.dataPath + "/../" + ScreenshotsFolder;
+            if (!System.IO.Directory.Exists(ScreenshotsPath))
+                System.IO.Directory.CreateDirectory(ScreenshotsPath);
+
+            string TimeNow = System.DateTime.Now.ToString("yyyyMMddHHmmssFFF");
+            string Path    = ScreenshotsPath + "/" + TimeNow + ".png";
+
+            ScreenCapture.CaptureScreenshot(Path, Scale);
+
+            m_ScreenshotsTaken++;
+        }
+	}
+}
