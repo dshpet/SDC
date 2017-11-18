bc88b3f13f5c0b39df0ed1952229acf7cc9b0c68
dshpet <dshpet@gmail.com>
2017-11-18 19:36:41 +0200

    slightly adjust streets, add new free asset packs

---
+++ UnityModel/Assets/Scripts/Camera/OffsetMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class OffsetMovement : MonoBehaviour
+{
+    public  GameObject PositionProvider;
+    private Vector3    m_Offset;
+
+    void Start()
+    {
+        m_Offset = transform.position - PositionProvider.transform.position;
+    }
+	
+	void LateUpdate()
+    {
+        transform.position = PositionProvider.transform.position + m_Offset;
+        transform.rotation = PositionProvider.transform.rotation; // todo check with LookAt
+        // http://wiki.unity3d.com/index.php/SmoothFollow2
+    }
+}
