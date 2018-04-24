f7a7cf760f43fd5bf68e57d867d5ddef5df9bed6
Dmitry Shpetnyi <shpetnyi@g5software.com>
2018-04-24 21:19:10 +0300

    add input simulation

---
+++ Driving/src/inputsimulator.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python 
+
+'''
+Input simulation to surpass Direct input method needed for some simulators
+[Windows only]
+
+Useful sources
+http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
+http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+
+pyautogui (not the latest version as a lot of packages change)
+https://anaconda.org/conda-forge/pyautogui
+'''
+
+import ctypes
+import pyautogui
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+# todo use proper keycodes
+W = 0x11
+A = 0x1E
+S = 0x1F
+D = 0x20
+
+#
+# C struct redefinitions 
+#
+
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+  _fields_ = [
+      ("wVk",         ctypes.c_ushort),
+      ("wScan",       ctypes.c_ushort),
+      ("dwFlags",     ctypes.c_ulong ),
+      ("time",        ctypes.c_ulong ),
+      ("dwExtraInfo", PUL            )
+    ]
+
+class HardwareInput(ctypes.Structure):
+  _fields_ = [
+      ("uMsg",    ctypes.c_ulong ),
+      ("wParamL", ctypes.c_short ),
+      ("wParamH", ctypes.c_ushort)
+    ]
+
+class MouseInput(ctypes.Structure):
+  _fields_ = [
+      ("dx",          ctypes.c_long ),
+      ("dy",          ctypes.c_long ),
+      ("mouseData",   ctypes.c_ulong),
+      ("dwFlags",     ctypes.c_ulong),
+      ("time",        ctypes.c_ulong),
+      ("dwExtraInfo", PUL           )
+    ]
+
+class Input_I(ctypes.Union):
+  _fields_ = [
+      ("ki", KeyBdInput   ),
+      ("mi", MouseInput   ),
+      ("hi", HardwareInput)
+    ]
+
+class Input(ctypes.Structure):
+  _fields_ = [
+      ("type", ctypes.c_ulong),
+      ("ii",   Input_I       )
+    ]
+
+#
+# Interface
+#
+
+def PressKey(_hexKeyCode):
+  extra  = ctypes.c_ulong(0)
+  ii_    = Input_I()
+  ii_.ki = KeyBdInput(0, _hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+  x      = Input(ctypes.c_ulong(1), ii_)
+
+  ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(_hexKeyCode):
+  extra  = ctypes.c_ulong(0)
+  ii_    = Input_I()
+  ii_.ki = KeyBdInput(0, _hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+  x      = Input(ctypes.c_ulong(1), ii_)
+
+  ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def PressKeyIndirect(_key):
+  pyautogui.keyDown(_key)
+
+def ReleaseKeyIndirect(_key):
+  pyautogui.keyUp(_key)
+
+#
+# Test
+#
+
+# if __name__ == '__main__':
+#     PressKey(0x11)
+#     time.sleep(1)
+#     ReleaseKey(0x11)
+#     time.sleep(1)
--- Driving/src/main.py
+++ Driving/src/main.py
@@ -8,11 +8,20 @@ Run through Anaconda python distrib on Windows 10
 
 OpenCV
 https://stackoverflow.com/questions/23119413/how-do-i-install-python-opencv-through-conda
+
+pyautogui (not the latest version as a lot of packages change)
+https://anaconda.org/conda-forge/pyautogui
 '''
 
 import numpy as np
 import cv2
+import time
 from PIL import ImageGrab
+import inputsimulator as input
+
+# todo use it on other simulators with direct input processing
+# terminal takes the input
+# from inputsimulator import PressKey, W, A, S, D
 
 def process_image(_image):
   processed_image = cv2.cvtColor(_image, cv2.COLOR_BGR2GRAY)
@@ -20,15 +29,17 @@ def process_image(_image):
 
   return processed_image
 
-
 def main():
   while True:
+    # todo change
+    input.PressKeyIndirect('w') 
+
     # 800x600 window
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
     processed_screen = process_image(screen)
 
-    cv2.imshow('window', processed_screen)
-    #cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imshow('SDC_driver', processed_screen)
+    #cv2.imshow('SDC_driver', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
       cv2.destroyAllWindows()
